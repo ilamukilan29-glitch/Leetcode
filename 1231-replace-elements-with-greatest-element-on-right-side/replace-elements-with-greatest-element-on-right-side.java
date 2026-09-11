7a44b73aa8411ed471d434468620a4f4a911d574
Ilamukilan <ilamukilan29@gmail.com>
2026-09-11 21:03:24 +0530

    Time: 1416 ms (5.17%) | Memory: 49 MB (64.04%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            if(i==arr.length-1){
+                arr[i]=-1;
+                continue;
+            }
+            int max = 0;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>max){
+                    max=arr[j];
+                }
+            }
+            arr[i]=max;
+        }
+        return arr;
+    }
+}
